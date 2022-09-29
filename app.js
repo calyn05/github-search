58d7ca4e686b20a08d03dec1782607e39af03b61
calyn05 <67522453+calyn05@users.noreply.github.com>
2022-09-29 13:48:34 +0300

    ready to deploy

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 //  Get elements
 
 const avatar = document.getElementById("profile-img");
